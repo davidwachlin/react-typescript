51bddb845f21e591328c8a4a8b82c3071c3d30a5
David Wachlin <davidwachlin@gmail.com>
2019-12-23 23:44:08 -0500

    adds basic types to app.js

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,28 @@ import './App.css';
 class App extends Component {
 	render() {
 		let firstValue: string = 'Manny';
+		let secondValue: number = 34;
+		let thirdValue: boolean = true;
+		let fourthValue: number[] = [2, 3, 45];
+		let fifthValue: Array<string> = ['2', 'cat', 'David'];
 		return (
 			<div className='App'>
 				<header className='App-header'>
 					<img src={logo} className='App-logo' alt='logo' />
 					<p>
 						The value {firstValue} is of {typeof firstValue} type!
+					</p>
+					<p>
+						The value {secondValue} is of {typeof secondValue} type!
+					</p>
+					<p>
+						The value {thirdValue} is of {typeof thirdValue} type!
+					</p>
+					<p>
+						The value {fourthValue} is of {typeof fourthValue} type!
+					</p>
+					<p>
+						The value {fifthValue} is of {typeof fifthValue} type!
 					</p>
 				</header>
 			</div>
