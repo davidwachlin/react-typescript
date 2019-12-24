7efc070781d2f0f0d18ed775037d0b63c334ee07
David Wachlin <davidwachlin@gmail.com>
2019-12-23 23:58:24 -0500

    adds complex data types to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,34 @@ import './App.css';
 
 class App extends Component {
 	render() {
-		let firstValue: string = 'Manny';
-		let secondValue: number = 34;
-		let thirdValue: boolean = true;
-		let fourthValue: number[] = [2, 3, 45];
-		let fifthValue: Array<string> = ['2', 'cat', 'David'];
+		// let firstValue: string = 'Manny';
+		// let secondValue: number = 34;
+		// let thirdValue: boolean = true;
+		// let fourthValue: number[] = [2, 3, 45];
+    // let fifthValue: Array<string> = ['2', 'cat', 'David'];
+
+    // tuple
+    let aTuple: [string, number] = ['Manny', 34];
+
+    // enum
+    enum Codes { first = 1, second = 2};
+
+    // any
+    let firstName: any = 'Manny';
+
+    // void
+    const warning = (): void => {
+      console.log('warning')
+    }
+
 		return (
 			<div className='App'>
 				<header className='App-header'>
 					<img src={logo} className='App-logo' alt='logo' />
 					<p>
+						The value {aTuple[0]} is of {typeof aTuple[0]} type!
+					</p>
+					{/* <p>
 						The value {firstValue} is of {typeof firstValue} type!
 					</p>
 					<p>
@@ -27,7 +45,7 @@ class App extends Component {
 					</p>
 					<p>
 						The value {fifthValue} is of {typeof fifthValue} type!
-					</p>
+					</p> */}
 				</header>
 			</div>
 		);
